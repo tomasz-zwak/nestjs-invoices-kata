8f6f71efa34d7f87ca1b1934d8e796bc18966fba
tomasz-zwak <t.zwak@selleo.com>
2021-12-27 15:42:45 +0100

    initial commit

---
+++ src/invoices/invoices.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { InvoicesService } from './invoices.service';
+
+@Controller('invoices')
+export class InvoicesController {
+  constructor(private readonly invoicesService: InvoicesService) {}
+
+  @Get()
+  index() {
+    return 'abc';
+  }
+}
