87ed0e598a4368bbf084c4b7b4c27b98104d1d94
tomasz-zwak <t.zwak@selleo.com>
2022-01-20 12:52:35 +0100

    graphql api for invoices

--- src/invoices/dto/update-invoice.dto.ts
+++ src/invoices/dto/update-invoice.dto.ts
@@ -1,4 +1,7 @@
+import { InputType, PartialType as PartialTypeGql } from '@nestjs/graphql';
 import { PartialType } from '@nestjs/swagger';
 import { CreateInvoiceDto } from './create-invoice.dto';
 
 export class UpdateInvoiceDto extends PartialType(CreateInvoiceDto) {}
+@InputType()
+export class UpdateInvoiceDtoGql extends PartialTypeGql(CreateInvoiceDto) {}
--- src/invoices/invoices.resolver.ts
+++ src/invoices/invoices.resolver.ts
@@ -1,10 +1,15 @@
+import { ParseIntPipe, UseInterceptors } from '@nestjs/common';
 import { Args, ID, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from '../auth/decorators/user.decorator';
 import { User } from '../user/entities/user.entity';
 import { CreateInvoiceDto } from './dto/create-invoice.dto';
 import { InvoiceApproveDto } from './dto/invoice-approve.dto';
+import { UpdateInvoiceDtoGql } from './dto/update-invoice.dto';
 import { Invoice } from './entities/invoice.entity';
 import { InvoicesService } from './invoices.service';
+import { PdfResponse } from '../pdf/pdf.type';
+import { InvoicePdfMetadata } from './invoice.dto';
+import { InvoiceDownloadInterceptor } from '../commons/invoice-download.interceptor';
 
 @Resolver(() => Invoice)
 export class InvoicesResolver {
@@ -48,6 +53,30 @@ export class InvoicesResolver {
     return this.invoicesService.create(invoiceDto, user);
   }
 
-  updateInvoice() {}
-  deleteInvoice() {}
+  @Mutation(() => Invoice)
+  updateInvoice(
+    @Args({ name: 'invoiceId', type: () => Int }) id: number,
+    @Args('invoice') invoiceDto: UpdateInvoiceDtoGql,
+    @CurrentUser() user: User,
+  ) {
+    return this.invoicesService.update(id, invoiceDto, user);
+  }
+
+  @Mutation(() => Invoice)
+  async deleteInvoice(
+    @Args({ name: 'invoiceId', type: () => Int }) id: number,
+    @CurrentUser() user: User,
+  ) {
+    const invoice = await this.invoicesService.delete(id, user);
+    invoice.id = id;
+    return invoice;
+  }
+
+  @UseInterceptors(InvoiceDownloadInterceptor)
+  @Query(() => InvoicePdfMetadata)
+  download(
+    @Args({ name: 'invoiceId', type: () => Int }) id: number,
+  ): InvoicePdfMetadata {
+    return { id };
+  }
 }
