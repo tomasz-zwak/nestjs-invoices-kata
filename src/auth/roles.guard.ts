e10a144cb2bb3a9b69d0db464e94363e555f92d3
tomasz-zwak <t.zwak@selleo.com>
2022-01-25 15:49:52 +0100

    update RolesGuard to work with grqphql requests

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { Role, UserPayload } from '../user/user.type';
@@ -13,7 +14,8 @@ export class RolesGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const roles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler());
-    const request = context.switchToHttp().getRequest<Request>();
+    let request = context.switchToHttp().getRequest<Request>();
+    if (!request) request = GqlExecutionContext.create(context).getContext();
     const user = request.user as UserPayload;
     return roles.includes(user.role);
   }
